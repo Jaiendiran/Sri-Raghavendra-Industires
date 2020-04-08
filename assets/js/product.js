33831630669539a0fff47b3de8eecca8820cb5a5
Jaiendiran M <jaiendiran2494@gmail.com>
2020-04-08 14:41:56 +0530

    Updates

---
+++ assets/js/product.js
@@ -0,0 +1,6 @@
+//Modal
+
+// $('#modl_nav_btn').on('click', function(e) {
+//     e.preventDefault();
+//     $('#staticBackdrop').modal('show').find('.modal-content').load($(this).attr('href'));
+// });
