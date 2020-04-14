adcd12abec17ebb785d8674b6b0393f838f45901
Jaiendiran M <jaiendiran2494@gmail.com>
2020-04-14 08:34:08 +0530

    product finalized and ready for hosting.

--- dist/assets/js/product.js
+++
@@ -1,6 +0,0 @@
-//Modal
-
-// $('#modl_nav_btn').on('click', function(e) {
-//     e.preventDefault();
-//     $('#staticBackdrop').modal('show').find('.modal-content').load($(this).attr('href'));
-// });
